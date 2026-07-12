1b574ceb86a9786876e2e8b8bfbbd0b0d1713994
Sanskar-12 <sanskarv2004@gmail.com>
2026-07-12 17:56:35 +0530

    Feature: Added redux for user state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
 import Home from "./pages/Home";
 import { getCurrentUser } from "./features/getCurrentUser";
+import { useDispatch } from "react-redux";
+import { setUserData } from "./redux/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const fetchCurrentUser = () => {
-      getCurrentUser();
+    const fetchCurrentUser = async () => {
+      const data = await getCurrentUser();
+      dispatch(setUserData(data.user));
     };
 
     fetchCurrentUser();
-  }, []);
+  }, [dispatch]);
   return (
     <>
       <Home />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,17 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../../utils/firebase";
 import api from "../../utils/axios";
 import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../redux/userSlice";
 
 const Home = () => {
+  const { userData } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   const googleLogin = async () => {
     const data = await signInWithPopup(auth, googleProvider);
     const token = await data.user.getIdToken();
     await loginHandler(token);
-    console.log(data);
   };
 
   const loginHandler = async (token) => {
@@ -16,7 +20,7 @@ const Home = () => {
       const { data } = await api.post("/api/auth/login", {
         token,
       });
-      console.log(data);
+      dispatch(setUserData(data.user));
     } catch (error) {
       console.log(error);
     }
@@ -24,25 +28,27 @@ const Home = () => {
 
   return (
     <div className="h-screen flex bg-[#0d0f14] text-white overflow-hidden">
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur">
-        <div className="w-[#340px] bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
-          <div className="flex flex-col gap-1">
-            <h2 className="text-[17px] font-semibold text-slate-100 tracking-tight">
-              Welcome to CortexAI
-            </h2>
-            <p className="text-[13px] text-slate-500">
-              Please login to continue using the app.
-            </p>
+      {!userData && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur">
+          <div className="w-[#340px] bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
+            <div className="flex flex-col gap-1">
+              <h2 className="text-[17px] font-semibold text-slate-100 tracking-tight">
+                Welcome to CortexAI
+              </h2>
+              <p className="text-[13px] text-slate-500">
+                Please login to continue using the app.
+              </p>
+            </div>
+            <button
+              className="w-full flex items-center justify-center gap-3 py-2.75 rounded-xl text-sm font-medium text-black/70 bg-white hover:bg-gray-200 transition-all duration-150 cursor-pointer"
+              onClick={googleLogin}
+            >
+              <FcGoogle size={15} />
+              Continue with Google
+            </button>
           </div>
-          <button
-            className="w-full flex items-center justify-center gap-3 py-2.75 rounded-xl text-sm font-medium text-black/70 bg-white hover:bg-gray-200 transition-all duration-150 cursor-pointer"
-            onClick={googleLogin}
-          >
-            <FcGoogle size={15} />
-            Continue with Google
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
